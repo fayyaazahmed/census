ecae9597d7c9433855f7dd9c8c80889a6241ca56
fayyaazahmed <fayahm14@gmail.com>
2023-05-29 22:07:44 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# census
+# Cenus
+Finds the subset of populations that sum to the target value.
+
+
+## Requirements
+- `Python 2.7`
+
+## Usage
+```
+python census.py [-h] <input_file> <target>
+```
+
+## Arguments
+### Required Arguments
+`input_file`: Specifies the path to the CSV file to be processed.
+`target`: Specifies the target value for the subset problem.
+### Optional Arguments
+`-h`, `--help`: Shows help message and exit.
+
+## Input Format
+The input file must be in CSV format (.csv) and contain the following field(s):
+
+`populations`: Score of the student (integer).
+
+## Output Format
+The program outputs the top student(s) in the following format to STDOUT (see result_example.txt):
+```
+Target: <target>
+Populations: [populations[0], populations[1], populations[n]]
+```
+For output to .txt file use piping: 
+```
+python census.py input_files/sample.csv > result_sample.txt
+```
+
+
+## Example Usage
+The following command processes sample.csv and outputs the subset of populations to the command line:
+
+```
+python census.py input_files/sample.csv 101000000
+```
+
+
+## Limitations
+
+- 
---
+++ census.py
@@ -0,0 +1,72 @@
+import sys, argparse, csv
+from itertools import combinations
+
+def read_csv(file_name):
+    
+    populations = []
+    
+    with open(file_name, 'r') as file:
+        csv_file = csv.reader(file)
+        for row in csv_file:
+            for item in row:
+                try:
+                    number = int(item)
+                    populations.append(number)
+                except ValueError:
+                    pass  # Skip non-integer values
+                except IOError:
+                    # catches erroneous file names
+                    sys.stdout.write("Could not read file \'" + file_name + "\'\n")
+                    sys.exit()
+
+    return populations
+
+
+def find_subset(nums, target):
+    for r in range(1, len(nums) + 1):
+        for subset in combinations(nums, r):
+            if sum(subset) == target:
+                return list(subset)
+    
+    # No subset found. sad :(
+    return []
+
+
+def main():
+    """
+    Parses command-line arguments and runs the program to read in a csv file of populations, then sort TODO!!!!
+    
+    Command-line arguments:
+    -h, --help: show this help message and exit
+    input_file: required positional argument; specifies the CSV file of populations to read (Format: count (integer))
+    target:     required value to sum to.
+
+    Returns:    String output
+    """
+    # basic CLI interface for better accessiblity
+    parser = argparse.ArgumentParser(
+        description="Census - Finds the subset of populations that sum to the target",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("input_file", help="CSV file of populations (Format: count (integer)")
+    parser.add_argument("target", help="Target value to sum to.")
+    args = parser.parse_args()
+    config = vars(args)
+
+    populations = read_csv(config['input_file'])
+    target = int(config['target'])
+    subset = find_subset(populations, target)
+
+    # returns and exits for empty set
+    if len(populations) < 1:
+        sys.stdout.write("No values were found in \'" + config['input_file'] + "\'\n")
+        sys.exit()
+
+    if subset:
+        print('Target:\t\t' + str(target))
+        print('Subset:\t\t' + str(subset))
+    else:
+        print("No subset found with a total population of ", target)
+
+
+if __name__ == "__main__":
+    main()
